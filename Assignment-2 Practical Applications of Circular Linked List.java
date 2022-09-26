088db0546093cc3cda564e8d5fde1f21934fcea7
yogeshgupta0201 <97100739+yogeshgupta0201@users.noreply.github.com>
2022-09-26 23:29:54 +0530

    Update Assignment-2 Practical Applications of Circular Linked List.java

--- Assignment-2 Practical Applications of Circular Linked List.java
+++ Assignment-2 Practical Applications of Circular Linked List.java
@@ -1,8 +1,11 @@
 Some of the practical applications of Circular Linked Lists are-
 Implementation of circular scheduling in high speed graphics.
+When more than one task is given by the user, then the division of CPU time for each process is decided by the operating system using Circular Linked Lists.
 Scheduling token rings in computer networks.
 Many of the multiplayer games use circular linked list to swap between players in a loop.
 The Browser cache which allows you to hit back the button.
 Undo functionality in Photoshop or Word.
 These are widely used in Round Robin Scheduling.
 It has wide application in implementation of Advanced Data Structures such as Fibonacci Heap. 
+In a music player, the circular linked list is used to keep track of the songs. Songs in the music player are linked to the previous and next songs. you can play songs either from starting or ending of the list.
+  
